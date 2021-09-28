4ff0097fbf7687f1cb0cd5a1da7c52035acccbfd
Ilias <iliasd752@gmail.com>
2021-09-28 21:41:55 +0200

    check post-container component

--- src/app/modules/admin/dashboards/posts/post-container.component.ts
+++ src/app/modules/admin/dashboards/posts/post-container.component.ts
@@ -11,7 +11,7 @@ export class PostContainerComponent implements OnInit {
 
     public rows: Posts[];
     public columns = [
-         {userId: 'userId', id: 'id'}
+         {id: 'id'}
     ];
 
     constructor(private interactionService: InteractionService) {
